8141fd7be8f54ca166221e03a7f513be87ecafa5
DESKTOP-NUPROA5\Андрей <areikyn16@gmail.com>
2021-02-06 18:13:56 +0300

    Implemented class BTree and BTreePrinter

---
+++ BinaryTreeSearch/BinaryTree/BNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class BNode<T> where T : IComparable
+    {
+        public int item;
+        public BNode<T> right;
+        public BNode<T> left;
+
+        public BNode(int item)
+        {
+            this.item = item;
+        }
+    }
+}
---
+++ BinaryTreeSearch/Program.cs
@@ -0,0 +1,24 @@
+﻿using BinaryTreeSearch;
+using System;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BTree btr = new BTree();
+            btr.Add(6);
+            btr.Add(2);
+            btr.Add(3);
+            btr.Add(11);
+            btr.Add(30);
+            btr.Add(9);
+            btr.Add(13);
+            btr.Add(18);
+            btr.Add(27);
+
+            btr.Print();
+        }    
+    }
+}
